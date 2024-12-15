08a3d753d59b193b0a3a13d786799b4b5cac1746
David Briscoe <idbrii@gmail.com>
2024-12-15 10:14:38 -0800

    Add simple anim tester
    Allow you to test your anims within this project to see how it works.

---
+++ tester/animtester.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+@onready var sprite := $AnimatedSprite2D
+@onready var template := $"%ButtonTemplate"
+
+
+func _ready():
+    _build_anims()
+
+
+func _build_anims():
+    # Remove old buttons.
+    var root = template.get_parent()
+    for child in root.get_children():
+        if child != template:
+            child.queue_free()
+
+    var anims = sprite.sprite_frames.get_animation_names()
+    for anim_name in anims:
+        var btn = template.duplicate()
+        root.add_child(btn)
+        btn.visible = true
+        btn.text = anim_name
+        btn.pressed.connect(_on_pressed_anim_btn.bind(anim_name))
+
+
+func _on_pressed_anim_btn(anim_name):
+    sprite.play(anim_name)
